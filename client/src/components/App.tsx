53277975a8e504f41acc2b6cf9175f99d4194c45
Piotr Rydwański <pioryd123@gmail.com>
2021-01-31 09:13:21 +0100

    Client cleanup

---
+++ client/src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Switch>
+        </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
